4b2c17bf28d43635d499dc18ef92e5e1bf65253d
David Corticchiato <dawi@drone>
2014-01-28 18:32:31 +0100

    manage in/out bodies

--- basil.js
+++ basil.js
@@ -4,12 +4,14 @@
  * Module dependencies.
  */
 
+var url = require('url');
 var http = require('http');
 
 function Basil() {
   if (!(this instanceof Basil)) return new Basil;
 
   this.handlers = [];
+  
 }
 
 /**
@@ -33,66 +35,93 @@ Basil.prototype.use = function (handler) {
  */
 
 Basil.prototype.listen = function (port) {
+  var proxy = http.createServer();
+
+  proxy.on('request', this.wakeup.bind(this));
+  proxy.listen(port);
+};
+
+/**
+ * On request event : init bundle
+ */
+
+Basil.prototype.wakeup = function (clientRequest, response) {
   var handlers = this.handlers;
-  
-  http.createServer(function (reqA, resA) {
-    var reqB = {};
+  var bundle = {request: {}, reqBody: '', resBody: ''};
 
-    hydrate(reqA, reqB, handlers);
-    http.request(reqB, grab(reqA, reqB, resA, handlers)).end();
-  }).listen(port);
+  clientRequest.on('data', function (buf) {
+    bundle.reqBody += buf.toString();
+  });
+
+  clientRequest.on('end', function () {
+    repeat(bundle, clientRequest, response, handlers);
+  });
 };
 
 /**
- * Execute all the middlewares to hydrate the request reqB
+ * On request end : start repeating server response
  */
 
-var hydrate = function (reqA, reqB, handlers) {
+var repeat = function (bundle, clientRequest, response, handlers) {
+  hydrate(bundle, clientRequest, handlers);
+  var proxyRequest = http.request(bundle.request, function(serverResponse) {
+    serverResponse.on('data', function (buf) {
+      bundle.resBody += buf.toString();
+    });
+
+    serverResponse.on('end', function (buf) {
+      answer(bundle, clientRequest, response, serverResponse, handlers);
+    });
+  });
+
+  proxyRequest.write(bundle.request.body);
+  proxyRequest.end();
+};
+
+/**
+ * Write response
+ */
+
+var answer = function (bundle, clientRequest, response, serverResponse, handlers) {
+  dehydrate(bundle, serverResponse, handlers);
+  response.writeHeader(bundle.response.status, bundle.response.headers);
+  response.write(bundle.response.body);
+  response.end();
+};
+
+/**
+ * Build request bundle from origin and middlewares
+ */
+
+var hydrate = function (bundle, request, handlers) {
+  bundle.request = url.parse(request.url);
+  bundle.request.headers = request.headers;
+  bundle.request.method = request.method;
+  bundle.request.agent = false;
+  bundle.request.body = bundle.reqBody;
+
+  whisper(bundle, handlers);
+};
+
+/**
+ * Build response bundle from origin and middlewares
+ */
+
+var dehydrate = function (bundle, response, handlers) {
+  bundle.response = {};
+  bundle.response.status = response.statusCode;
+  bundle.response.headers = response.headers;
+  bundle.response.body = bundle.resBody;
+
+  whisper(bundle, handlers);
+};
+
+/**
+ * Exec all the middlewares
+ */
+
+var whisper = function (bundle, handlers) {
   handlers.forEach(function (handler) {
-    handler(reqA, reqB, null, null, null);
-  });
-};
-
-/**
- * Returns the request grabber
- */
-
-var grab = function (reqA, reqB, resA, handlers) {
-  return function (resB) {
-    var bundle = {data: ''};
-
-    feed(resB, bundle);
-    end(reqA, reqB, resA, resB, bundle, handlers);
-  };
-};
-
-/**
- * Feed the data from the server response
- */
-
-var feed = function (resB, bundle) {
-  resB.on('data', function (buffer) {
-    bundle.data += buffer.toString();
-  });
-};
-
-/**
- * Execute all the middlewares to dehydrate the response resB
- */
-
-var dehydrate = function (reqA, reqB, resA, resB, bundle, handlers) {
-  handlers.forEach(function (handler) {
-    handler(reqA, reqB, resA, resB, bundle.data);
-  });
-};
-
-/**
- * Listen the 'end' event and close the response resA
- */
-
-var end = function (reqA, reqB, resA, resB, bundle, handlers) {
-  resB.on('end', function () {
-    dehydrate(reqA, reqB, resA, resB, bundle, handlers);
-    resA.end();
+    handler(bundle);
   });
 };
